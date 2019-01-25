846331cec62ac9bbecf3b1642b31a933f3e3e654
lt <136148586@qqcom>
2019-01-25 17:10:38 +0800

    2019.1.25 17:10

--- javaBasic/src/com/javaBasic/basicGrammar/basicCollectionAndMap/Student.java
+++ javaBasic/src/com/javaBasic/basicGrammar/basicCollectionAndMap/Student.java
@@ -6,29 +6,34 @@ package com.javaBasic.basicGrammar.basicCollectionAndMap;
  * @Description
  */
 public class Student implements Comparable {
-    private int age;
-    private String name;
+	private int age;
+	private String name;
 
-    public Student(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
+	public Student(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
 
-    @Override
-    public int compareTo(Object o) {
-        if (!(o instanceof Student)) {
-            throw new RuntimeException("不 Student 对象");
-        }
-        Student b = (Student) o;
-        if (this.age > b.age) {
-            return 1;
-        }
-        if (this.age == b.age) {
-            return this.name.compareTo(b.name);
-        }
-        if (this.age < b.age) {
-            return -1;
-        }
-        return 0;
-    }
+	@Override
+	public int compareTo(Object o) {
+		if (!(o instanceof Student)) {
+			throw new RuntimeException("不 是Student 对象");
+		}
+		Student b = (Student) o;
+		if (this.age > b.age) {
+			return 1;// 1表示顺序排列
+		}
+		if (this.age == b.age) {
+			return this.name.compareTo(b.name);
+		}
+		if (this.age < b.age) {
+			return -1;// -1倒序排列
+		}
+		return 0;// 0表示该元素重复，不可再添加。
+	}
+
+	@Override
+	public String toString() {
+		return "姓名:" + this.name + "年龄:" + this.age;
+	}
 }
--- javaBasic/src/com/javaBasic/basicGrammar/basicCollectionAndMap/TestCollection.java
+++ javaBasic/src/com/javaBasic/basicGrammar/basicCollectionAndMap/TestCollection.java
@@ -1,6 +1,13 @@
 package com.javaBasic.basicGrammar.basicCollectionAndMap;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Vector;
 
 /**
  * @Author: LT
@@ -48,26 +55,42 @@ public class TestCollection {
 	// set无序不重复，最常用的两大实现HashSet、TreeSet
 	// HashSet不会保证元素的顺序，
 	public static void testHashSet() {
-
+		Set<Integer> set1 = new HashSet<>();
+		set1.add(1);
+		set1.add(1);
+		set1.add(4);
+		set1.add(1);
+		System.out.println(set1);
 	}
 
-	// TreeSet会默认把里面的元素排序
+	// TreeSet会默认把里面的元素排序,底层是红黑二叉树，存储规则：左小右大
 	public static void testTreeSet() {
-		//自然怕排序
-		TreeSet<Integer> treeSet=new TreeSet<>();
+		// 自然怕排序
+		TreeSet<Integer> treeSet = new TreeSet<>();
 		treeSet.add(5);
 		treeSet.add(4);
 		treeSet.add(1);
-		//System.out.println(treeSet);
-		TreeSet<Object> treeSet1=new TreeSet<>();
-		treeSet1.add(new Student(12,"cc"));
-		treeSet1.add(new Student(15,"aa"));
-		treeSet1.add(new Student(12,"dd"));
-		treeSet1.add(new Student(12,"bb"));
-		System.out.println(treeSet1);
+		// System.out.println(treeSet);
+
+		// 传入元素不具备自然排序规则，该元素类必须实现comparable接口实现compareTo（）方法
+		TreeSet<Object> treeSet1 = new TreeSet<>();
+		treeSet1.add(new Student(12, "cc"));
+		treeSet1.add(new Student(12, "aa"));
+		treeSet1.add(new Student(12, "dd"));
+		treeSet1.add(new Student(12, "bb"));
+		// System.out.println(treeSet1.toString());
+
+		// 传入元素类没有实现comparable接口，元素也不具备自然表规则，那就在创建treeset的时候传入比较器。
+		PeopleComparator pc = new PeopleComparator();
+		TreeSet<Object> treeSet2 = new TreeSet<>(pc);
+		treeSet2.add(new People("bb", 11));
+		treeSet2.add(new People("aa", 133));
+		treeSet2.add(new People("aa", 112));
+		treeSet2.add(new People("cc", 14));
+		System.out.println(treeSet2.toString());
 	}
 
 	public static void main(String[] args) {
-		testTreeSet();
+		testHashSet();
 	}
 }
